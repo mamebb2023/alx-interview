854ce73715a74fbda3549805fb5668a289208b29
Mohammednur <mamez7878@gmail.com>
2023-12-03 19:01:02 +0300

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+        triangle.append(row)
+    for row in triangle:
+        print(row)
