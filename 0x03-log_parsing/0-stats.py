5dadb30ef33b142473b366ce6cd817f0d8da96db
Mohammednur <mamebb2023@gmail.com>
2023-12-21 20:31:22 +0300

    Log Report

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" Log Parsing """
-
-import re
+""" Log Report """
 import sys
+import re
 
 
-def stat_print(log: dict) -> None:
-    """ prints stats """
+def output(log: dict) -> None:
+    """ Display stats """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
         if log["code_frequency"][code]:
@@ -15,13 +14,13 @@ def stat_print(log: dict) -> None:
 
 if __name__ == "__main__":
     regex = re.compile(
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
     lines = 0
     log = {}
     log["file_size"] = 0
     log["code_frequency"] = {
-        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
+            str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
         }
 
     try:
@@ -32,13 +31,12 @@ if __name__ == "__main__":
                 lines += 1
                 code = match.group(1)
                 file_size = int(match.group(2))
-
                 log["file_size"] += file_size
 
                 if (code.isdecimal()):
                     log["code_frequency"][code] += 1
 
                 if (lines % 10 == 0):
-                    stat_print(log)
+                    output(log)
     finally:
-        stat_print(log)
+        output(log)
