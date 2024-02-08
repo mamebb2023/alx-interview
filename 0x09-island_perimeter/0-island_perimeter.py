3ded0d1c723b15b63ee0070d7bf39d877379fa05
Mohammednur <mamebb2023@gmail.com>
2024-02-08 11:40:03 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,26 +6,25 @@
 def island_perimeter(grid):
     """ Returns the perimeter of a rectangular island
     """
-    row_idx = None
-    land = None
-
+    row_index = None
+    island_index = None
     for i, row in enumerate(grid):
         if 1 in row:
-            land = row.index(1)
-            row_idx = i
+            island_index = row.index(1)
+            row_index = i
             break
 
-    if row_idx is None or land is None:
+    if row_index is None or island_index is None:
         return 0
 
     width = 0
-    for row in grid[row_idx:]:
-        if row[land] != 1:
+    for row in grid[row_index:]:
+        if row[island_index] != 1:
             break
         width += 1
 
     length = 0
     for i in range(width):
-        length = max(length, grid[land + i].count(1))
+        length = max(length, grid[island_index + i].count(1))
 
-    return 2 * (width + length)
+    return (2 * width) + (2 * length)
